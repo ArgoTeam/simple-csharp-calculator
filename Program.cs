79d32f9c2bf163e07b586c433b73f51aa418115d
ArgoTeam <61902510+ArgoTeam@users.noreply.github.com>
2022-01-21 19:40:44 +0100

    Sqrt

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Simple_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj liczbę");
+            string number;
+            number = Console.ReadLine();
+            int num;
+            num = Convert.ToInt32(number);
+            Console.WriteLine(Math.Sqrt(num));
+        }
+    }
+}
